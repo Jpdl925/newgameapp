5ec2d5de2c11bc8caf5b73624ab5cf5c7a4eb1a1
Jpdl925 <jtpg925@gmail.com>
2024-06-13 20:05:23 -0700

    New edits

--- newgameappl/src/App.tsx
+++ newgameappl/src/App.tsx
@@ -1,5 +1,6 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react"
 import NavBar from "./components/NavBar"
+import GameGrid from "./components/GameGrid"
 
 const App = () => {
   return (
@@ -16,11 +17,15 @@ const App = () => {
       </GridItem>
 
       <Show above="lg">
-      <GridItem area="aside">Aside</GridItem>
+      <GridItem area="aside">
+        Aside
+        </GridItem>
 
       </Show>
 
-      <GridItem area="main">Main</GridItem>
+      <GridItem area="main">
+        <GameGrid/>
+        </GridItem>
     </Grid>
 
 
--- newgameappl/src/services/apiClient.ts
+++ newgameappl/src/services/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-axios.create({
-    baseURL:'',
+export default axios.create({
+    baseURL:'https://api.rawg.io/api',
     params:{
         key:'749a832b94924ccf9b9bd189d240fb2d'
     }
